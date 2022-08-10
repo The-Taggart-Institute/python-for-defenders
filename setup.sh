e585938d259551805f2e84a3cc24b8cb116aa657
Michael Taggart <michael.t.taggart@gmail.com>
2022-08-09 19:03:51 -0700

    Add python3-venv and poetry

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@
 echo "[+] Installing dependencies"
 
 sudo apt update
-sudo apt install -y python3-pip
+sudo apt install -y python3-pip python3-venv
 sudo pip3 install jupyterlab plotly ipywidgets pandas jupyter-dash
 
+echo "[+] Installing Poetry"
+
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
