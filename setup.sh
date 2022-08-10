2e33349aa93828297091a025f0f71e70aa19db55
Michael Taggart <michael.t.taggart@gmail.com>
2022-08-09 19:34:46 -0700

    Update setup for poetry

--- setup.sh
+++ setup.sh
@@ -4,8 +4,12 @@ echo "[+] Installing dependencies"
 
 sudo apt update
 sudo apt install -y python3-pip python3-venv
-sudo pip3 install jupyterlab plotly ipywidgets pandas jupyter-dash
 
 echo "[+] Installing Poetry"
 
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+
+echo "[+] Installing Poetry Dependencies"
+poetry install
+
+echo "[+] Done! Run 'poetry shell' to activate the environment"
