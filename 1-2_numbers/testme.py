b4fc2c9f744b12e5e11c2a0e2bf774247168f247
Michael Taggart <michael.t.taggart@gmail.com>
2022-08-10 21:38:52 -0700

    Finish 1-2

--- 1-2_numbers/testme.py
+++ 1-2_numbers/testme.py
@@ -1,8 +1,13 @@
 def testme_1(data):
-    pass
+    print("[+] Testing for floatiness...")
+    if type(data) == float:
+        print(f"[+] {data} is a float!")
+    else:
+        print(f"[!] {data} is not a float!")
 
-def testme_2(data):
-    pass
-
-def testme_3(data):
-    pass
+def testme_2(x, y):
+    print("[+] Testing for modiness...")
+    if x & y == 5:
+        print(f"[+] Success! {x} % {y} is 5!")
+    else:
+        print(f"[!] Hmm, {x} % {y} does not yield a remainder of 5...")
