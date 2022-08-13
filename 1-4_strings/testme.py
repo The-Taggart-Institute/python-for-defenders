6e6ab662e89c56d02776872d09170c4d9ee4cc32
Michael Taggart <michael.t.taggart@gmail.com>
2022-08-12 21:13:21 -0700

    1-4

---
+++ 1-4_strings/testme.py
@@ -0,0 +1,22 @@
+def testme_1(data):
+    print("[+] Testing foo...")
+    if data == "!sdrawkcab eb dluohs I":
+        print("[+] Succesfully reversed!")
+    else:
+        print(f"[-] {data} doesn't seem to match what I expected.")
+        
+
+def testme_2(data):
+    print("[+] Testing bar...")
+    if data == "hipporwhil":
+        print("[+] Slice successful!")
+    else:
+        print(f"[-] {data} doesn't seem to match what I expected.")
+        
+
+def testme_3(data):
+    print("[+] Testing baz...")
+    if data == "The values x and y are 77 and 28":
+        print("[+] Formatting successful!")
+    else:
+        print(f"[-] {data} doesn't seem to match what I expected.")
