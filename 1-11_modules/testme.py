631828dea1a2f24a3ee268bdada965a8bec512ba
Michael Taggart <michael.t.taggart@gmail.com>
2022-08-29 16:04:47 -0700

    Finish 1-11

---
+++ 1-11_modules/testme.py
@@ -0,0 +1,50 @@
+import os
+
+def testme():
+    # Check to see if the file exists
+    print("[+] Making sure we have indicator.py")
+    try:
+        os.stat("indicator.py")
+        print("[+] Module found")
+    except FileNotFoundError:
+        print("[!] Hey what gives?! I don't even have an indicator.py file!")
+        return
+    
+    # Import the stuff we want
+    print("[+] Importing our classes")
+    try:
+        from indicator import URLIndicator, DomainIndicator, IPv4Indicator
+    except ImportError as e:
+        print(f"[!] {e}")
+        return
+    
+    # Make sane indicators
+    print("[+] Making IP indicator")
+    try:
+        bad_ip = IPv4Indicator("192.168.99.1")
+        print(f"[+] {bad_ip.defang()}")
+    except:
+        print("[!] Couldn't make/defang IP indicator!")
+        return
+        
+    print("[+] Making domain indicator")
+    try:
+        bad_domain = IPv4Indicator("evil.com")
+        print(f"[+] {bad_ip.defang()}")
+    except:
+        print("[!] Couldn't make/defang domain indicator!")
+        return
+        
+    print("[+] Making URL indicator")
+    try:
+        bad_url = URLIndicator("https://taggart-tech.com")
+        print(f"[+] {bad_url.defang()}")
+    except:
+        print("[!] Couldn't make/defang URL indicator!")
+        return
+        
+        
+    
+
+    
+    
