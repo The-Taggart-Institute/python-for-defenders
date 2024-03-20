a316ff8ce6cc8beffea9b599592298997f48e30a
Michael Taggart <mtaggart@taggart-tech.com>
2024-03-19 20:05:35 -0700

    Update Poetry install

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ sudo apt install -y python3-pip python3-venv
 
 echo "[+] Installing Poetry"
 
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+curl -sSL https://install.python-poetry.org | python3 -
 
 echo "[+] Installing Poetry Dependencies"
 poetry install
